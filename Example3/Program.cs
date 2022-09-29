1137c85b53554b2617e6f55713ffdcdebfbb2f09
maribogl <maribogl@yandex.ru>
2022-09-29 15:43:29 +0300

    приме3 SUM

---
+++ Example3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите число А");
+string numberA = Console.ReadLine();
+int number1 = int.Parse(numberA);
+Console.Write("введите число B");
+string numberB = Console.ReadLine();
+int number2 = int.Parse(numberB);
+int result = number1 + number2;
+Console.Write("Cумма равна");
+Console.Write(result);
