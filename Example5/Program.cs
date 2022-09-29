59b00c44999777004aabafa8fc56a9099889cb01
maribogl <maribogl@yandex.ru>
2022-09-29 16:26:33 +0300

    приме5 masha

---
+++ Example5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите имя");
+string name = Console.ReadLine();
+if( name.ToLower() == "маша" ){
+    Console.WriteLine("Ура, это же Маша!");
+} else{
+    Console.Write("Privet,");
+    Console.Write(name);
+}
