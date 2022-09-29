a8a47217b7abcd644d0ee09c2e425996f84cebb6
maribogl <maribogl@yandex.ru>
2022-09-29 16:05:58 +0300

    приме4 random

--- Example4/Program.cs
+++ Example4/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA=12;
-double numberB=5;
-Console.Write(numberA/numberB);
+﻿double numberA= new Random().Next(1,10);// случайное число от 1 до 9 включительно
+Console.WriteLine(numberA);
+double numberB=new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA/numberB);
